0b605b1b7c7cdeacab479f1ad9a476597c6e26d9
admin <admin@qq.com>
2019-12-31 15:43:43 +0800

    first commit

---
+++ src/com/igeek/app/App.java
@@ -0,0 +1,14 @@
+package com.igeek.app;
+
+/**
+ * @Author: ASUS
+ * @Date :2019/12/31
+ * @Modify By:ASUS
+ * @Description :
+ * @Version :
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
